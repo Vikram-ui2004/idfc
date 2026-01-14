d7eae19a462c3541e97babf8fe08036c58f2c83a
Vikram Nayak <nayakvikram038@gmail.com>
2026-01-14 14:31:06 +0530

    m

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Navbar from '../components/Navbar';
+import Navbar from "@/components/Navbar";
+
 import HeroBanner from "@/components/HeroBanner";
 import CardSection from "@/components/CardSection";
 
